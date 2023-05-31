6947e6c2e815fcc0e0126fad6710caf8d6fefa41
Carlos Gamero <cgamero@unique-yanbal.com>
2023-05-31 10:30:21 -0500

    App registro horas equipo

---
+++ app.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import streamlit as st
+from azure.cosmos import exceptions, CosmosClient, PartitionKey
+import json
+
+######################################################################
+## CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+######################################################################
+
+######################################################################
+## DATABASE
+# conectarnos a la instancia
+endpoint = "https://registrohoras.documents.azure.com:443/"
+key = 'i4Jsp0MGQdqQY4QQvhJKM7SIJ2856GrzPmycWQjJOgEWDS93o8zjLwA1neNtGXkcaRyLc2PbdXGfACDbb06FEg=='
+client = CosmosClient(endpoint, key)
+DATABASE_NAME = 'controlhoras'
+CONTAINER_NAME_PROYECTOS = 'Proyectos'
+CONTAINER_NAME_POTENCIALESPROYECTOS = 'PotencialesProyectos'
+CONTAINER_NAME_HORAS = 'RegistroHoras'
+CONTAINER_EQUIPO = 'Equipo'######################################################################
+
+######################################################################
+database = client.get_database_client(DATABASE_NAME)
+container_proy = database.get_container_client(CONTAINER_NAME_PROYECTOS)
+container_potproy = database.get_container_client(CONTAINER_NAME_POTENCIALESPROYECTOS)
+container_horas = database.get_container_client(CONTAINER_NAME_HORAS)
+container_equipo = database.get_container_client(CONTAINER_EQUIPO)
+
+item_list_equipo = list(container_equipo.read_all_items())
+item_list_pot_proy = list(container_potproy.read_all_items())
+item_list_proy = list(container_proy.read_all_items())
+lista_equipo = [item['equipo'] for item in item_list_equipo]
+######################################################################
+
+######################################################################
+df_proyectos_equipo = pd.DataFrame(columns=['Tipo','Proyecto']+lista_equipo)
+cont = 0
+for item in item_list_proy:
+    proyecto_id = item['id']
+    if 'Run' in proyecto_id:
+        tipo = 'Run'
+    else:
+        tipo = 'Pared'
+    nombre = item['nombre']
+    equipo = item['equipo']
+    lista_participacion_proyecto = []
+    for per in lista_equipo:
+        if per in equipo:
+            lista_participacion_proyecto.append(1)
+        else:
+            lista_participacion_proyecto.append(0)
+    lista_temp = [tipo,nombre] + lista_participacion_proyecto
+    df_proyectos_equipo.loc[cont] = lista_temp
+    cont = cont + 1
+    
+for item in item_list_pot_proy:
+    tipo = item['estado']
+    nombre = item['nombre']
+    equipo = item['equipo']
+    lista_participacion_proyecto = []
+    for per in lista_equipo:
+        if per in equipo:
+            lista_participacion_proyecto.append(1)
+        else:
+            lista_participacion_proyecto.append(0)
+    lista_temp = [tipo,nombre] + lista_participacion_proyecto
+    df_proyectos_equipo.loc[cont] = lista_temp
+    cont = cont + 1
+
+st.markdown('### Lista de todos los proyectos del equipo')
+st.dataframe(df_proyectos_equipo)
